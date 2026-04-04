e719739a25c06da892aba06840e85151ec7a33b1
makarim muhammad <jakamakarim@gmail.com>
2026-04-05 02:24:41 +0700

    feat : add product routes

--- src/routes/products.routes.js
+++ src/routes/products.routes.js
@@ -1,8 +1,9 @@
 import express from "express";
 import productsController from "../controllers/products.controller.js";
-
 import variantsController from "../controllers/variants.controller.js";
 import sizesController from "../controllers/sizes.controller.js";
+import productImagesController from "../controllers/product-images.controller.js";
+import productDiscountsController from "../controllers/product-discounts.controller.js";
 
 const router = express.Router();
 
@@ -22,5 +23,17 @@ router.delete("/:id/variants/:variantId", (req, res) => variantsController.remov
 router.get("/:id/sizes", (req, res) => sizesController.getSizesByProduct(req, res));
 router.post("/:id/sizes", (req, res) => sizesController.addSizeToProduct(req, res));
 router.delete("/:id/sizes/:sizeId", (req, res) => sizesController.removeSizeFromProduct(req, res));
+
+// Product Images routes
+router.get("/:id/images", (req, res) => productImagesController.getImagesByProduct(req, res));
+router.post("/:id/images", (req, res) => productImagesController.addImageToProduct(req, res));
+router.put("/:id/images/:imageId", (req, res) => productImagesController.updateImage(req, res));
+router.delete("/:id/images/:imageId", (req, res) => productImagesController.deleteImage(req, res));
+
+// Product Discounts routes
+router.get("/:id/discounts", (req, res) => productDiscountsController.getDiscountsByProduct(req, res));
+router.post("/:id/discounts", (req, res) => productDiscountsController.createDiscount(req, res));
+router.put("/:id/discounts/:discountId", (req, res) => productDiscountsController.updateDiscount(req, res));
+router.delete("/:id/discounts/:discountId", (req, res) => productDiscountsController.deleteDiscount(req, res));
 
 export default router;
