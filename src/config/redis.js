c48dc0adae961d3cf6b8443dc6421481b2bd989e
makarim muhammad <jakamakarim@gmail.com>
2026-04-15 16:41:49 +0700

    feat: change redis configuration file to match updated version

--- src/config/redis.js
+++ src/config/redis.js
@@ -1,8 +1,10 @@
 import redis from 'redis';
 
 const redisClient = redis.createClient({
-  host: process.env.REDIS_HOST || 'localhost',
-  port: process.env.REDIS_PORT || 6379,
+  socket: {
+    host: process.env.REDIS_HOST || 'localhost',
+    port: process.env.REDIS_PORT || 6379,
+  },
   password: process.env.REDIS_PASSWORD || undefined,
   db: 0,
 });
